9d8abbb383cc060fd8ee190d95fdbcf40880a7e7
Javie1127 <javie.lew@gmail.com>
2021-03-11 12:35:18 -0600

    new step size rules

--- BCD_AP Algorithm/WMMSE_MRMC/Subgradient/tsp_DL_subgradient.m
+++ BCD_AP Algorithm/WMMSE_MRMC/Subgradient/tsp_DL_subgradient.m
@@ -12,10 +12,10 @@ cov_temp = cov;
 fdcomm_op = fdcomm;
 radar_op = radar;
 cov_op = cov;
-lambda_d_k_t = fdcomm.lambda_DL(k);
-mu_dj_k_t = fdcomm.mu_DL(jj,k);
-% lambda_d_k_t = 10;
-% mu_dj_k_t = 1;
+% lambda_d_k_t = fdcomm.lambda_DL(k);
+% mu_dj_k_t = fdcomm.mu_DL(jj,k);
+lambda_d_k_t = 10;
+mu_dj_k_t = 50;
 d_DL_j = fdcomm.DLstream_num(jj);
 HBj = fdcomm.DLchannels{jj};
 % R_in_dj = cov.in_DL{jj,k};
@@ -27,36 +27,48 @@ Xi_op = zeros(td_max,1);
 
 % Xi_min = fdcomm_temp.Xi_UL(k) + fdcomm_temp.Xi_DL(k) + sum(radar_temp.Xi_r);
 % U_dj_k = fdcomm_temp.DL_WMMSE_RX{jj,k};
-Xi_min = fdcomm_temp.Xi_WMMSE_total;
-while t <= td_max
-    
-    epsilon_j_k_t = 1/sqrt(t);
-    % update lambda_k_t
-    P_dg_k_sum = 0;
-    for g = 1:J
-        P_dg_k = fdcomm_temp.DLprecoders{g,k};
-        if g ~= jj
-            P_dg_k_sum = abs(trace((P_dg_k*P_dg_k'))) + P_dg_k_sum;
-        end
+Xi_min = fdcomm_temp.xi_UL(k)+fdcomm_temp.xi_DL(k)+sum(radar.Xi_radar_Nr);
+P_dg_k_sum = 0;
+for g = 1:J
+    P_dg_k = fdcomm_temp.DLprecoders{g,k};
+    if g ~= jj
+        P_dg_k_sum = abs(trace((P_dg_k*P_dg_k'))) + P_dg_k_sum;
     end
-    P_dj_k = fdcomm_temp.DLprecoders{jj,k};
-    P_B_t = P_dg_k_sum + abs(trace((P_dj_k*P_dj_k')));
-    beta_d_k_t = 1/(t);
-    %beta_d_k_t = 1/t/norm((P_B_t-P_B_max));
-    lambda_k_d_temp = lambda_d_k_t + beta_d_k_t*(P_B_t-P_B_max);
-    lambda_k_d_temp = max(0,lambda_k_d_temp);
-    fdcomm_temp.lambda_DL(k) = lambda_k_d_temp;
-    %% update mu_j_k_t
-%     E_jd_k_t = fdcomm.DL_MMSE{jj,k};
-%     R_jd_k_t = log2(det((E_jd_k_t)^(-1)));
-    R_in_dj = cov_temp.in_DL{jj,k};
+end
+while t <= td_max
+    Xi_t = fdcomm_temp.Xi_WMMSE_total;
+    tilde_P_dj_k = fdcomm_op.DLprecoders{jj,k};
     P_dj_k_t = fdcomm_temp.DLprecoders{jj,k};
-    R_dj_k_t = abs(log2(det(eye(d_DL_j)+(P_dj_k_t'*HBj'/R_in_dj*HBj*P_dj_k_t))));
-    mu_jd_k_temp = mu_dj_k_t + epsilon_j_k_t*(R_DL-R_dj_k_t);
+    P_B_t = P_dg_k_sum + abs(trace((P_dj_k_t*P_dj_k_t')));
+    g_t = P_B_t-P_B_max;
+    R_in_dj = cov_temp.in_DL{jj,k};
+    R_dj_k_t = abs(log2(det(eye(d_DL_j)+nearestSPD(P_dj_k_t'*HBj'/R_in_dj*HBj*P_dj_k_t))));
+    q_t = R_DL-R_dj_k_t;
+    switch fdcomm.step_size_rules
+        case 'Square_summable'
+            epsilon_j_k_t = 1/t;
+            beta_d_k_t = 1/(t);
+        case 'Nonsummable_diminishing'
+            epsilon_j_k_t = (1/sqrt(t))/(norm(q_t));
+            beta_d_k_t = (1/sqrt(t))/(norm(g_t)+1e-3*P_B_max);
+        case 'Polyak'
+            gamma = 0.5;
+            epsilon_j_k_t = (Xi_t-Xi_min+gamma^t)/(norm(q_t))^2;
+            beta_d_k_t = (Xi_t-Xi_min+gamma^t)/(norm(g_t))^2;        
+    end
+    % update lambda_k_t
+    
+    lambda_k_d_temp = lambda_d_k_t + beta_d_k_t*(P_B_t-P_B_max);
+    %lambda_k_d_temp = max(0,min(lambda_k_d_temp,lambda_d_k_t));
+    lambda_d_k_t = max(0,lambda_k_d_temp);
+    fdcomm_temp.lambda_DL(k) = lambda_d_k_t;
+    %% update mu_j_k_t
+    
+    mu_jd_k_temp = mu_dj_k_t + epsilon_j_k_t*q_t;
     mu_dj_k_t = max(mu_jd_k_temp,0);
     fdcomm_temp.mu_DL(jj,k) = mu_dj_k_t;
     %% update P_jd_k
-    fdcomm_temp = tsp_DL_precoders(k, jj, fdcomm_temp, cov_temp);
+    fdcomm_temp = tsp_DL_precoders(k, jj, fdcomm_temp, cov_temp,tilde_P_dj_k);
 %     fdcomm_temp = Comm_MMSE(fdcomm_temp,radar,cov_temp); 
 %     radar_temp = radar_MMSE(radar_temp,cov_temp);
     %% Update the MSE matrix E_dj_k_t
@@ -66,19 +78,18 @@ while t <= td_max
 %     E_dj_k_t = eye(fdcomm.DLstream_num(jj))-2*U_dj_k*HBj*P_dj_k_t+(U_dj_k*R_DL_j_k*U_dj_k');
 %     fdcomm_temp.DL_MMSE_nop{jj,k} = E_dj_k_t;
     cov_temp = update_cov_dj_k(fdcomm_temp,radar_temp,cov_temp,radar_comm,P_dj_k_t,jj,k);
-    [fdcomm_temp,radar_temp] = update_Xi_WMMSE(fdcomm_temp, radar_temp,cov_temp);
+    [fdcomm_temp,radar_temp] = update_Xi_WMMSE_k(fdcomm_temp, radar_temp,cov_temp,k);
     Xi_t = fdcomm_temp.Xi_WMMSE_total;
     %% update Xi_MSE
 %     fdcomm_temp = Xi_comm_k(fdcomm_temp, k);
 % %     radar_temp = Xi_radar(radar_temp);
 %     Xi_t = fdcomm_temp.Xi_UL(k)+ fdcomm_temp.Xi_DL(k) + sum(radar_temp.Xi_r);
     Xi(t) = Xi_t;
-    lambda_j_k(t) = lambda_k_d_temp;
+    lambda_j_k(t) = lambda_d_k_t;
     mu_j_k(t) = mu_dj_k_t;
     P_dj_k_temp = fdcomm_temp.DLprecoders{jj,k};
     power_temp = abs(trace((P_dj_k_temp*P_dj_k_temp'))) + P_dg_k_sum;
-    R_in_dj_temp = cov_temp.in_DL{jj,k};
-    R_dj_k_t_temp = abs(log2(det(eye(d_DL_j)+(P_dj_k_temp'*HBj'/R_in_dj_temp*HBj*P_dj_k_temp))));
+    R_dj_k_t_temp = abs(log2(det(eye(d_DL_j)+(P_dj_k_temp'*HBj'/R_in_dj*HBj*P_dj_k_temp))));
     if Xi_t < Xi_min && power_temp <= P_B_max && R_dj_k_t_temp >= R_DL
         Xi_min = Xi_t; 
         Xi_op(t) = Xi_t; 
@@ -87,6 +98,9 @@ while t <= td_max
         cov_op = cov_temp;
     else
         Xi_op(t) = Xi_min;
+    end
+    if Xi_t>10^2*Xi_min
+        break
     end
     t = t+1;
 end
