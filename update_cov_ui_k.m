1961a1bc9c42830b414b21d7801d76ea38e57581
Liu <jxl165430@utdallas.edu>
2021-03-13 21:36:19 -0600

    remove nearestPSd

--- update_cov_ui_k.m
+++ update_cov_ui_k.m
@@ -7,7 +7,9 @@ H_iB = H_UL{ii};
 P_uI = fdcomm.ULprecoders;
 P_iB_k = P_uI{ii,k};
 R_iB_old = R_IB{ii,k};
-R_iB_k = nearestSPD(H_iB*(P_iB_k*P_iB_k')*H_iB');
+R_iB_k = (H_iB*(P_iB_k*P_iB_k')*H_iB');
+d_new = eye(size(R_iB_k), 'logical');
+R_iB_k(d_new) = abs(diag(R_iB_k));
 R_IB{ii,k} = R_iB_k;
 I = fdcomm.UL_num;
 R_MUI_UL = cov.MUI_UL ;
@@ -54,7 +56,7 @@ for nr = 1:Nr
                R_i_nr(m,l) = abs(trace(s_inr_m*s_inr_l'*Sigma_U_Nr{m,l,ii,nr}));
            end
         end
-        R_i_nr = nearestSPD(R_i_nr);
+        %R_i_nr = nearestSPD(R_i_nr);
         R_U_Nr{ii,nr} = R_i_nr;
         R_U_nr_temp = R_U_nr_temp + R_i_nr;
     end
@@ -76,12 +78,12 @@ if fdcomm.DL_num > 0
     J = fdcomm.DL_num;
     for jj = 1:J
         Hij = H_UL_DL{ii,jj};
-        R_ij_old = nearestSPD(Hij*(P_iB_k_old*P_iB_k_old')*Hij');
-%         d_old =  eye(size(R_ij_old), 'logical');
-%         R_ij_old(d_old) = abs(diag(R_ij_old));
-        R_ij_new = nearestSPD(Hij*(P_iB_k*P_iB_k')*Hij');
-%         d_new = eye(size(R_ij_new), 'logical');
-%         R_ij_new(d_new) = abs(diag(R_ij_new));
+        R_ij_old = (Hij*(P_iB_k_old*P_iB_k_old')*Hij');
+        d_old =  eye(size(R_ij_old), 'logical');
+        R_ij_old(d_old) = abs(diag(R_ij_old));
+        R_ij_new = (Hij*(P_iB_k*P_iB_k')*Hij');
+        d_new = eye(size(R_ij_new), 'logical');
+        R_ij_new(d_new) = abs(diag(R_ij_new));
         R_in_DL{jj,k} = R_in_DL{jj,k}-R_ij_old+ R_ij_new;
         R_ULDL{jj,k} = R_ULDL{jj,k}-R_ij_old+ R_ij_new;
         R_total_DL{jj,k} = R_total_DL{jj,k}-R_ij_old+R_ij_new;
